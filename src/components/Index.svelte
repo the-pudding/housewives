e0c27256c92b740f120eadef7aef3bc4d4524445
Michelle Pera-McGhee <michmac202@gmail.com>
2024-12-04 17:27:12 -0800

    Initial commit

---
+++ src/components/Index.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { getContext } from "svelte";
+	import Footer from "$components/Footer.svelte";
+
+	// const copy = getContext("copy");
+	// const data = getContext("data");
+</script>
+
+<!-- <Footer recirc={true} /> -->
