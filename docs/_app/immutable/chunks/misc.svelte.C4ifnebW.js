f392a46599aeefe1ac54bafb23950eae9c977932
Michelle McGhee <michmac202@gmail.com>
2025-02-05 18:46:03 -0800

    update github pages

--- docs/_app/immutable/chunks/misc.svelte.C4ifnebW.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./proxy.CjYWIFVS.js";const s=e({section:0,slide:0,subslide:0,slideInSection:0}),n=e({id:void 0});export{s as c,n as m};
--- docs/_app/immutable/entry/start.BJKpK4fc.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D70zOvdq.js";export{t as start};
