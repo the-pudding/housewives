070d723eb9b54ade317639bc5d5b9375ce446ae8
Michelle McGhee <michmac202@gmail.com>
2025-02-25 17:43:26 -0800

    update github pages

--- docs/_app/immutable/chunks/index-client.BCtRj6TM.js
+++
@@ -1,1 +0,0 @@
-import{e as t,l as e,u as c,f}from"./runtime.Ce5bzpHM.js";function s(o){t===null&&e(),c(()=>{const n=f(o);if(typeof n=="function")return n})}export{s as o};
---
+++ docs/_app/immutable/chunks/misc.svelte.ClzTlM-c.js
@@ -0,0 +1,1 @@
+import{p as s}from"./proxy.BExY56DC.js";const n=s({section:0,slide:0,subslide:0,slideInSection:0}),i=s({id:void 0}),o=s({ccOn:!1,soundOn:!0});export{n as c,i as m,o as v};
