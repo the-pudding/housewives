de8d1f79e925ef501c4d1526cc9a54b8391e2729
Michelle McGhee <michmac202@gmail.com>
2025-03-06 18:09:42 -0800

    update github pages

---
+++ docs/_app/immutable/chunks/misc.svelte.C1lj1M6G.js
@@ -0,0 +1,1 @@
+import{p as s}from"./proxy.BExY56DC.js";const n=s({section:0,slide:0,subslide:0,slideInSection:0}),o=s({id:void 0}),t=s({ccOn:!1,soundOn:!0}),a=s({open:!1});export{a,n as c,o as m,t as v};
